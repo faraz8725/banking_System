82ae481ec3123996885ab2b95c7f1f87f5a15c76
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-03-22 23:01:26 +0530

    line 25

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -20,3 +20,28 @@ const decoded=jwt.verify(token,process.env.JWT_SECRET)
     })
   }
 }
+
+
+/
+const userModel=require("../models/user.model");
+const jwt=require("jsonwebtoken");
+const emailService=require("../services/email.service")
+
+
+
+async function authMiddleware(req,res,next){
+
+  const token=req.cookies.token || req.headers.authorization?.split("")[1]
+  if(!token){
+    return res.status(401).json({
+      message:"Unauthorized access,token is missing"
+    })
+  }
+  try{
+const decoded=jwt.verify(token,process.env.JWT_SECRET)
+  }catch(err){
+    return res.status(401).json({
+      message:"Unauthorized access,token is invalid"
+    })
+  }
+} */
