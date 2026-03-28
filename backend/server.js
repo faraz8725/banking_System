a0bea96736ac7cd793cb961a3d1888cc4890a855
Faraz Sarfaraz Agha <fagha4697@gmail.com>
2026-03-28 23:09:52 +0530

    final commits

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-require("dotenv").config();
-const app=require("./src/app");
-const connectToDB=require("./src/config/db");
-connectToDB();
-PORT=process.env.PORT || 3000;
-app.listen(PORT,()=>{
-  console.log(`SERCER IS CONNECTED TO ${PORT}`)
-
-})
--- backend/src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const authController=require("../controllers/auth.controller")
-
-router.post("/register",authController.userRegisterController) // POST/API/AUTH/REGISTER
-router.post("/login",authController.userLoginController)
-
-module.exports=router;  // SOMETHING TIME PASS HAS BEEN ADDED
